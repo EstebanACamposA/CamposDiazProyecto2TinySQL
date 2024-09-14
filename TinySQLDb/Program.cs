e1c5292ea13a95e675ee779927a7a364acf86f6b
EstebanACamposA <est-campos@estudiantec.cr>
2024-09-14 16:31:33 -0600

    First commit.

---
+++ TinySQLDb/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Hello World!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
