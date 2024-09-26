eaa0fbbbf84f4b8eef46223b75eb34efb4a2e17a
JafetDM <148803642+JafetDM@users.noreply.github.com>
2024-09-25 19:42:50 -0600

    Update Arbol.cs

--- Arbol.cs
+++ Arbol.cs
@@ -1,13 +1,38 @@
 using System;
 
-public class Nodo<T>
+public class Fila : IComparable<Fila>
+{
+    public int ID { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+
+    public Fila(int id, string nombre, string apellido)
+    {
+        ID = id;
+        Nombre = nombre;
+        Apellido = apellido;
+    }
+
+    // Implementamos IComparable para que se pueda comparar por ID (o puedes cambiar el criterio)
+    public int CompareTo(Fila other)
+    {
+        return this.ID.CompareTo(other.ID);  // Comparación por ID
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {ID}, Nombre: {Nombre}, Apellido: {Apellido}";
+    }
+}
+
+public class Nodo
 {
     //atributos
-    T dato; 
+    Fila dato; 
     Nodo left;
     Nodo right;
 
-    public Nodo<T data>
+    public Nodo(Fila data)
     {
         dato = data;
         left = null;
@@ -16,14 +41,38 @@ public class Nodo<T>
 
 }
 
-public class Arbol <T>
+public class Arbol 
 {
     //atributos
-    Nodo <T> root;
+    Nodo root;
 
-    public Arbol<T>
+    public Arbol
     {
-        root = null;
+        root = null; //crea el arbol vacio
+    }
+
+    public void Insertar(Fila fila)
+    {
+        root = InsertarRecursivo(root, fila) //asigna la raiz a un metodo recursivo
+    }
+
+    private Nodo InsertarRecursivo(Nodo nodo, Fila fila)
+    {
+        if (nodo == null) //si el arbol esta vacio o si no se puede bajar mas en el arbol, el nodo se vuelve nulo y se le asigna el valor
+        {
+            nodo = new Nodo(fila);
+            return fila;
+        }
+
+        if (fila.CompareTo(nodo.data) < 0) //CompareTo devuelve un -1 si el elemento es menor al que se esta comparando
+        {
+            nodo.Izquierdo = InsertarRecursivo(nodo.Izquierdo, fila); //como es menor, desciende al hijo izquierdo y verifica recusrivamente otra vez.
+        }
+
+        else if (fila.CompareTo(nodo.Data) > 0) //CompareTo devuelve un 1 si el elemento es mayor
+        {
+            nodo.Derecho = InsertarRecursivo(nodo.Derecho, fila); // como es mayor, desciende al hijo derecho y verifica recusrivamente otra vez
+        }
     }
 
 }
